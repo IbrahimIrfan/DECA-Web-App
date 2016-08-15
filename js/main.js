acaccf354cf823083063dea4d009c1ccd4b8706e
IbrahimIrfan <1ibrahimirfan@gmail.com>
2016-08-14 22:31:52 -0400

    added things

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(document).on('ready', function(){
   $('#center-text').hide().delay(3000).fadeIn(1000);
-  $('#logo').hide().fadeIn(2000);
-  $('#logo').animate({right: '200px'}, 1000);
+  $('#logo').hide().fadeIn(2000).animate({right: '200px'}, 1000);
+  $('.footer').hide().delay(3000).fadeIn(1000);
   $('#cssmenu').hide().delay(3000).fadeIn(1000);
 });
