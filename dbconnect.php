f91f89ed23ac28e021fa4f44af8d21bc169cd596
IbrahimIrfan <1ibrahimirfan@gmail.com>
2016-08-29 23:13:45 -0400

    db configuration

--- dbconnect.php
+++ dbconnect.php
@@ -1,12 +1,12 @@
 <?php
 
- define('DBHOST', 'irhsdeca.com');
- define('DBUSER', '');
- define('DBPASS', '');
- define('DBNAME', 'members2016');
+ $host = $_GET["host"];
+ $user = $_GET["user"];
+ $pass = $_GET["pass"];
+ $name = $_GET["name"];
 
- $conn = mysql_connect(DBHOST,DBUSER,DBPASS);
- $dbcon = mysql_select_db(DBNAME);
+ $conn = mysql_connect($host,$user,$pass);
+ $dbcon = mysql_select_db($name);
 
  if ( !$conn ) {
   die("Connection failed : " . mysql_error());
@@ -17,3 +17,13 @@
 }
 
 ?>
+<html>
+<head>
+<script type="text/javascript">
+  $.get('/config.json', function(r){
+    var db = r;
+    window.location.href="/dbconnect.php?pass=" + db["dbpass"] + "&user=" + db["dbuser"] + "&name=" + db["dbname"] + "&host=" + db["dbhost"];
+  });
+</script>
+</head>
+</html>
