827453768e6b9afc459803d0be026524c96ef74e
IbrahimIrfan <1ibrahimirfan@gmail.com>
2016-09-03 17:52:34 -0400

    mobile css

--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,12 @@
-$(document).on('ready', function(){
-  $('#menu-bars').on('click', function(){
-    $('#mobile_menu ul').css('display', 'block');
-  });
+$(document).on('ready', function() {
+    $('#menu-bars').on('click', function() {
+        if ($('#mobile_menu ul').css('display') == 'block') {
+            $('#mobile_menu ul').css('display', 'none');
+        } else {
+            $('#mobile_menu ul').css('display', 'block');
+        }
+    });
+    $('.content').on('click', function(){
+      $('#mobile_menu ul').css('display', 'none');
+    });
 });
