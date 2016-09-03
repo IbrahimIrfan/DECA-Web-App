404772b60228e96165bced606405d0ba2d23ac14
IbrahimIrfan <1ibrahimirfan@gmail.com>
2016-09-03 18:14:32 -0400

    mobile css

--- js/menu.js
+++ js/menu.js
@@ -1,12 +1,12 @@
 $(document).on('ready', function() {
     $('#menu-bars').on('click', function() {
         if ($('#mobile_dropdown').is(':visible')) {
-            $('#mobile_dropdown').fadeOut(250);
+            $('#mobile_dropdown').fadeOut(150);
         } else {
-            $('#mobile_dropdown').fadeIn(250);
+            $('#mobile_dropdown').fadeIn(150);
         }
     });
     $('.content').on('click', function(){
-        $('#mobile_dropdown').fadeOut(250);
+        $('#mobile_dropdown').fadeOut(150);
     });
 });
