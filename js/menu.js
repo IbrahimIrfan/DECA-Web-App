17a4a783b2195bcb48fda8ea807ab912f6d06b75
IbrahimIrfan <1ibrahimirfan@gmail.com>
2016-09-03 18:11:56 -0400

    mobile css

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,6 @@
 $(document).on('ready', function() {
     $('#menu-bars').on('click', function() {
-        if ($('#mobile_dropdown').is(':visible') {
+        if ($('#mobile_dropdown').is(':visible')) {
             $('#mobile_dropdown').fadeOut(250);
         } else
             $('#mobile_dropdown').fadeIn(250);
