02041eabf714c26304106223090aa91d7d022884
IbrahimIrfan <1ibrahimirfan@gmail.com>
2016-09-08 19:44:57 -0400

    second test homework exam

--- Principles_2_Whole_Homework_Server.php
+++ Principles_2_Whole_Homework_Server.php
@@ -1,1 +1,1 @@
-<?phpob_start(); session_start(); require_once 'dbconnect.php'; $score = $_POST["score"]; $UID = $_SESSION['user']; $query = "INSERT INTO exams(userId, score_2, cluster) VALUES('$UID', '$score', 'Principles')" $res = mysql_query($query); ?>
+<?php ob_start(); session_start(); require_once 'dbconnect.php'; $score = $_POST["score"]; $UID = $_SESSION['user']; $query = "INSERT INTO exams(userId, score_2, cluster) VALUES('$UID', '$score', 'Principles')" $res = mysql_query($query); ?>
