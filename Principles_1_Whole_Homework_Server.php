51e160f4acf8152c7171a1f556f412f1b889b89f
IbrahimIrfan <1ibrahimirfan@gmail.com>
2016-09-08 18:47:24 -0400

    moved files

--- Principles_1_Whole_Homework_Server.php
+++ Principles_1_Whole_Homework_Server.php
@@ -4,6 +4,8 @@
   require_once 'dbconnect.php';
 
   $score = $_POST["score"];
-  $query = "INSERT INTO exams(userId, score_1, cluster) VALUES('4', '$score', 'Principles')";
+  $query = "DELETE FROM exams";
+  
+  //$query = "INSERT INTO exams(userId, score_1, cluster) VALUES('$_SESSION['user']', '$score', 'Principles')";
   $res = mysql_query($query);
 ?>
