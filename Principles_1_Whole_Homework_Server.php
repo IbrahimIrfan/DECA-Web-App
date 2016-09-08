870f7fd82f763ce9f72122836c23e7aa37c84ff8
IbrahimIrfan <1ibrahimirfan@gmail.com>
2016-09-08 18:40:27 -0400

    moved files

--- Principles_1_Whole_Homework_Server.php
+++ Principles_1_Whole_Homework_Server.php
@@ -1,7 +1,7 @@
 <?php
-  ob_start();
-  session_start();
-  require_once 'dbconnect.php';
+//  ob_start();
+//  session_start();
+//  require_once 'dbconnect.php';
 
   $score = $_POST["score"];
   $query = "INSERT INTO exams(userId, score_1, cluster) VALUES('3', '$score', 'Principles')";
