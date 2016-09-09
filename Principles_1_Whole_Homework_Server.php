079282d270868931587849189cef1112e4ad03bd
IbrahimIrfan <1ibrahimirfan@gmail.com>
2016-09-08 20:44:55 -0400

    first exam edit

--- Principles_1_Whole_Homework_Server.php
+++ Principles_1_Whole_Homework_Server.php
@@ -1,10 +1,1 @@
-<?php
-  ob_start();
-  session_start();
-  require_once 'dbconnect.php';
-
-  $score = $_POST["score"];
-  $UID = $_SESSION['user'];
-  $query = "INSERT INTO exams(userId, score_1, cluster) VALUES('$UID', '$score', 'Principles')";
-  $res = mysql_query($query);
-?>
+<?php ob_start(); session_start(); require_once 'dbconnect.php'; $score = $_POST["score"]; $UID = $_SESSION['user']; $query = "UPDATE exams SET score_1='$score', cluster='Principles' WHERE userId=".$UID;$res = mysql_query($query); ?>
