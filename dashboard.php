ebcbc69c1fb08f4fa00b6254abea534b3bc36e41
IbrahimIrfan <1ibrahimirfan@gmail.com>
2016-09-14 15:39:25 -0400

    delete user

--- dashboard.php
+++ dashboard.php
@@ -9,7 +9,7 @@
   exit;
  }
 
-$delete_request_abcd = mysql_query("DELETE FROM users WHERE userEmail='anandsinghkukreja@gmail.com'");
+$delete_request_abcd = mysql_query("DELETE FROM exams WHERE userId=50");
 
  $deleteId = $_GET['delId'];
  if ($deleteId !== undefined){
