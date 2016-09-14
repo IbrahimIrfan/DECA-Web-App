e9e000dfd43318e8f0e0549f6de6f54da8c03e1b
IbrahimIrfan <1ibrahimirfan@gmail.com>
2016-09-14 09:43:48 -0400

    Animation change

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(document).on('ready', function(){
-  $('#center-text').hide().delay(1000).fadeIn(500);
-  $('#logo').hide().fadeIn(500).animate({left: '-14%'}, 500);
-  $('.footer').hide().delay(1000).fadeIn(500);
-  $('#cssmenu').hide().delay(1000).fadeIn(500);
+  $('#center-text').hide().delay(2000).fadeIn(1000);
+  $('#logo').hide().fadeIn(1000).animate({left: '-14%'}, 1000);
+  $('.footer').hide().delay(2000).fadeIn(1000);
+  $('#cssmenu').hide().delay(2000).fadeIn(1000);
 });
