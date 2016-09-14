dc10c21fa365ca19a064d8c69aee91a27ed53646
IbrahimIrfan <1ibrahimirfan@gmail.com>
2016-09-14 15:48:11 -0400

    delete user

--- dashboard.php
+++ dashboard.php
@@ -198,7 +198,7 @@ $userRow["userEventAssigned"] == "AASM"|| $userRow["userEventAssigned"] == "BSM"
               <h4>Current Registration Status:<div color="green"> <?php
                // select exams detail
                $res_users_abc=mysql_query("SELECT * FROM users");
-              echo mysql_num_rows(mysql_fetch_array($res_users_abc));
+              echo mysql_num_rows($res_users_abc);
               ?></div></h4>
               <table class="mdl-data-table mdl-js-data-table" id="exam_scores"><thead>
               <tr><td width="100%">Name</td><td>Email</td><td>Event 1</td><td>Event 2</td><td>Event 3</td></tr></thead><tbody>
