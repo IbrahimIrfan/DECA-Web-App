debe8ede94a08a770b06ded8532f288d9588d2eb
IbrahimIrfan <1ibrahimirfan@gmail.com>
2016-09-16 21:53:10 -0400

    reset

--- dashboard.php
+++ dashboard.php
@@ -9,9 +9,6 @@
   exit;
  }
 
-   $delete_request_qqq = mysql_query('DELETE FROM users WHERE userId=587');
-     $delete_request_qqqq = mysql_query('DELETE FROM exams WHERE userId=587');
-
  $deleteId = $_GET['delId'];
  if ($deleteId !== undefined){
    $delete_request = mysql_query('DELETE FROM announcements WHERE announceId='.$deleteId);
