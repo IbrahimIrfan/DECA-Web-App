4d2b8c97ab232995d1d9d3ad11e0bc2d5d06ace1
IbrahimIrfan <1ibrahimirfan@gmail.com>
2016-09-19 19:19:29 -0400

    reset user

--- dashboard.php
+++ dashboard.php
@@ -8,8 +8,6 @@
   header("Location: login.php");
   exit;
  }
- $delete_request_bfwefew = mysql_query('DELETE FROM users WHERE userId=517');
-   $delete_request_qqqq = mysql_query('DELETE FROM exams WHERE userId=517');
 
  $deleteId = $_GET['delId'];
  if ($deleteId !== undefined){
