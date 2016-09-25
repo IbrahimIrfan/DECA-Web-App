95be0185634836ea97366025778cdc6adae96da0
IbrahimIrfan <1ibrahimirfan@gmail.com>
2016-09-25 00:07:40 -0400

    ann

--- dashboard.php
+++ dashboard.php
@@ -120,29 +120,30 @@ $exec = false;
         }elseif ($userRow_eee["userEventAssigned"] == "BLTDM" || $userRow_eee["userEventAssigned"] == "HRM"){
           $currentCluster_eee = "Business-Admin";
         }
+        $emailbody = "A new DECA ".$clusterManaging." announcement was posted:<br><br>".$title."<br><br>".$body."<br><br>More cluster-wide announcements: www.irhsdeca.com/dashboard.php<br><br>More chapter-wide announcements: www.irhsdeca.com/announcements.php<br><br>- The IRHS DECA Team";
 
+        require 'PHPMailer/PHPMailerAutoload.php';
+
+        $mail = new PHPMailer;
+
+        $mail->isSMTP();
+        $mail->Host = 'smtp.gmail.com';
+        $mail->SMTPAuth = true;
+        $mail->Username = 'irhsdeca2016@gmail.com';
+        $mail->Password = 'DECA2016';
+        $mail->SMTPSecure = 'tls';
+        $mail->Port = 587;
+
+        $mail->setFrom('irhsdeca2016@gmail.com', 'IRHS DECA');
+
+                 $mail->isHTML(true);           // Set email format to HTML
+
+                 $mail->Subject = 'New DECA Announcement: '. $title;
+                 $mail->Body    = $emailbody;
+      
         if ($currentCluster_eee == "Test"){
-          $emailbody = "A new DECA ".$clusterManaging." announcement was posted:<br><br>".$title."<br><br>".$body."<br><br>More cluster-wide announcements: www.irhsdeca.com/dashboard.php<br><br>More chapter-wide announcements: www.irhsdeca.com/announcements.php<br><br>- The IRHS DECA Team";
 
-          require 'PHPMailer/PHPMailerAutoload.php';
-
-          $mail = new PHPMailer;
-
-          $mail->isSMTP();
-          $mail->Host = 'smtp.gmail.com';
-          $mail->SMTPAuth = true;
-          $mail->Username = 'irhsdeca2016@gmail.com';
-          $mail->Password = 'DECA2016';
-          $mail->SMTPSecure = 'tls';
-          $mail->Port = 587;
-
-          $mail->setFrom('irhsdeca2016@gmail.com', 'IRHS DECA');
          $mail->addAddress($userRow_eee["userEmail"]);               // recipient
-
-         $mail->isHTML(true);           // Set email format to HTML
-
-         $mail->Subject = 'New DECA Announcement: '. $title;
-         $mail->Body    = $emailbody;
 
          if(!$mail->send()) {
            $errMSG = 'Mailer Error: ' . $mail->ErrorInfo;
