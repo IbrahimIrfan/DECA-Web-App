e657dc36d4539c93f7a228bc4cf4e965501cd2fb
IbrahimIrfan <1ibrahimirfan@gmail.com>
2016-09-29 18:38:03 -0400

    add event

--- dashboard2.php
+++ dashboard2.php
@@ -8,7 +8,7 @@
   header("Location: login.php");
   exit;
  }
- $add_event = mysql_query("UPDATE users SET userEventAssigned='PBM' WHERE userId=707");
+ 
  $deleteId = $_GET['delId'];
  if ($deleteId !== undefined){
    $delete_request = mysql_query('DELETE FROM announcements WHERE announceId='.$deleteId);
