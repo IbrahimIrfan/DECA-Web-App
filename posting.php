7d69a96cd80734149314ca419c37901fbec40c73
IbrahimIrfan <1ibrahimirfan@gmail.com>
2016-10-15 16:48:44 -0400

    uplaod

--- posting.php
+++ posting.php
@@ -6,7 +6,7 @@ require_once 'dbconnect.php';
 if(isset($_POST['submit'])) {
 
  $uplName = strip_tags(trim($_POST['name']));
- $uplsong = (int)strip_tags($_POST['song']);
+ $uplsong = (int)$_POST['song'];
 
 $error = false;
  if (empty($uplName) || empty($uplsong)){
