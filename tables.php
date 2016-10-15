2522f8d63bf1c380b3571fb706ca75cc54fd6c8d
IbrahimIrfan <1ibrahimirfan@gmail.com>
2016-10-15 16:05:19 -0400

    new table

---
+++ tables.php
@@ -0,0 +1,29 @@
+<?php
+
+
+  $json = file_get_contents('http://www.irhsdeca.com/config.json');
+  $details = json_decode($json, true);
+
+ $host = $details["dbhost"];
+ $user = $details["dbuser"];
+ $pass = $details["dbpass"];
+ $name = $details["dbname"];
+
+ $conn = mysql_connect($host,$user,$pass);
+ $dbcon = mysql_select_db($name);
+
+ $sql = "CREATE TABLE hackwestern (" +
+  "subId int(11) NOT NULL AUTO_INCREMENT," +
+  "uploaderName varchar(30) NOT NULL," +
+  "song int(11) NOT NULL "
+  " PRIMARY KEY (subId)" +
+ ") AUTO_INCREMENT=1 ;";
+
+$retval = mysql_query( $sql, $conn );
+if(! $retval )
+{
+  die('Could not create table: ' . mysql_error());
+}
+echo "Table created successfully\n";
+mysql_close($conn);
+?>
