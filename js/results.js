9d2397cac10a5c5588a34abe23aa71d4e4cfe1e9
IbrahimIrfan <1ibrahimirfan@gmail.com>
2016-10-24 10:59:33 -0400

    hmwk

--- js/results.js
+++ js/results.js
@@ -25,14 +25,13 @@ $(document).on('ready', function(){
           }
 
           scores.sort(sortNumber);
-          alert(scores.join(","));
 
           if (scores.length == 0) {
               $('#resultsHeading').html('No results found.');
               $('#resultsTable').html('');
           } else {
               for (i = scores.length - 1; i > -1; i--) {
-                  $('#resultsTable').append('<tr>' + '<td>' + scores[i][0] + '</td>' + '<td>' + scores[i][1] + '</td>' + '<td>' + scores[i][2] + '</td>' + '</tr>');
+                  $('#resultsTable').html('<tr><td>Overall</td><td>Exam</td><td>Case Study</td></tr>' + '<tr>' + '<td>' + scores[i][0] + '</td>' + '<td>' + scores[i][1] + '</td>' + '<td>' + scores[i][2] + '</td>' + '</tr>');
               }
           }
       });
