5aa010a62835f2ed4ab096dbd434d18827adc480
IbrahimIrfan <1ibrahimirfan@gmail.com>
2016-10-24 10:56:14 -0400

    hmwk

--- js/results.js
+++ js/results.js
@@ -1,31 +1,37 @@
-$('#submitEvent').on('click', function(){
-  $.get("/js/ftdm.txt", function(data) {
-    var line = data.split(" ");
+$('#submitEvent').on('click', function() {
+    $.get("/js/ftdm.txt", function(data) {
+        var line = data.split(" ");
 
-    var scores = []
+        var scores = []
 
-    var eventName = $('#eventInput').val();
+        var eventName = $('#eventInput').val();
 
-    for (i = 0; i < line.length; i++){
-        if (line[i].toUpperCase() == eventName.toUpperCase()){
-            if (line[i+6] == 'Hamilton' || line[i+5] == 'Hamilton'){
-                if (parseInt(line[i+1]) > 15){
-                    scores.push([parseInt(line[i+1]) + parseInt(line[i+2]), parseInt(line[i+1]), parseInt(line[i+2]), line[i]]);
-                }else{
-                    scores.push([parseInt(line[i+2]) + parseInt(line[i+3]), parseInt(line[i+2]), parseInt(line[i+3]), line[i]]);
-                  }
+        for (i = 0; i < line.length; i++) {
+            if (line[i].toUpperCase() == eventName.toUpperCase()) {
+                if (line[i + 6] == 'Hamilton' || line[i + 5] == 'Hamilton') {
+                    if (parseInt(line[i + 1]) > 15) {
+                        scores.push([parseInt(line[i + 1]) + parseInt(line[i + 2]), parseInt(line[i + 1]), parseInt(line[i + 2]), line[i]]);
+                    } else {
+                        scores.push([parseInt(line[i + 2]) + parseInt(line[i + 3]), parseInt(line[i + 2]), parseInt(line[i + 3]), line[i]]);
+                    }
                 }
+            }
         }
-    }
-    scores.sort();
 
-    if (scores.length == 0){
-      $('#resultsHeading').html('No results found.');
-      $('#resultsTable').html('');
-    }else{
-    for (i = scores.length - 1; i > -1; i--){
-      $('#resultsTable').append('<tr>' + '<td>' + scores[i][0] + '</td>' + '<td>' + scores[i][1] + '</td>' + '<td>' + scores[i][2] + '</td>' + '</tr>');
-    }
-  }
-  });
+        function sortNumber(a, b) {
+            return a - b;
+        }
+
+        scores.sort(sortNumber);
+        alert(scores.join(","));
+
+        if (scores.length == 0) {
+            $('#resultsHeading').html('No results found.');
+            $('#resultsTable').html('');
+        } else {
+            for (i = scores.length - 1; i > -1; i--) {
+                $('#resultsTable').append('<tr>' + '<td>' + scores[i][0] + '</td>' + '<td>' + scores[i][1] + '</td>' + '<td>' + scores[i][2] + '</td>' + '</tr>');
+            }
+        }
+    });
 });
