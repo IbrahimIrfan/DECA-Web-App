38ec581107ecf8b468fcce4876e71e4703635625
IbrahimIrfan <1ibrahimirfan@gmail.com>
2016-10-30 22:27:54 -0400

    nephew

--- thing.js
+++ thing.js
@@ -1,8 +1,8 @@
 $(document).on('ready', function() {
     for (var i = 1; i < 6; i++) {
-      var file = "/Finance_" + x + "_Whole_Master.php";
+        var file = "/Finance_" + i + "_Whole_Master.php";
         $.get(file, function(data) {
-          console.log($(data).find("h5"));
+            console.log($(data).find("h5"));
         });
     }
 });
