c9b16a958b13a4ac599a0b7c6644e3587ad2fad2
IbrahimIrfan <1ibrahimirfan@gmail.com>
2018-02-03 22:56:17 -0500

    exam comments

--- exams.php
+++ exams.php
@@ -1,13 +1,13 @@
 <?php
- ob_start();
- session_start();
- require_once 'dbconnect.php';
+ob_start();
+session_start();
+require_once 'dbconnect.php';
 
 // redirect to login if not logged in
- if( !isset($_SESSION['user']) ) {
-  header("Location: login.php");
-  exit;
- }
+if( !isset($_SESSION['user']) ) {
+	header("Location: login.php");
+	exit;
+}
 ?>
 
 <!--
@@ -36,20 +36,20 @@
 <html>
 
 <head>
-    <title> IRHS DECA </title>
+	<title> IRHS DECA </title>
 
-    <link rel="icon" href="img/favicon.ico" sizes="16x16">
-    <!-- Add jQuery library -->
-    <meta charset='utf-8'>
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="css/cssmenu/styles.css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-    <link rel="stylesheet" href="https://code.getmdl.io/1.1.3/material.blue_grey-red.min.css">
-    <link rel="stylesheet" type="text/css" href="css/main.css"></link>
-    <link rel="stylesheet" href="fancybox/source/jquery.fancybox.css?v=2.1.5" type="text/css" media="screen" />
-    <link rel="stylesheet" href="fancybox/source/helpers/jquery.fancybox-buttons.css?v=1.0.5" type="text/css" media="screen" />
-    <link rel="stylesheet" href="fancybox/source/helpers/jquery.fancybox-thumbs.css?v=1.0.7" type="text/css" media="screen" />
+	<link rel="icon" href="img/favicon.ico" sizes="16x16">
+	<!-- Add jQuery library -->
+	<meta charset='utf-8'>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="css/cssmenu/styles.css">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+	<link rel="stylesheet" href="https://code.getmdl.io/1.1.3/material.blue_grey-red.min.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css"></link>
+	<link rel="stylesheet" href="fancybox/source/jquery.fancybox.css?v=2.1.5" type="text/css" media="screen" />
+	<link rel="stylesheet" href="fancybox/source/helpers/jquery.fancybox-buttons.css?v=1.0.5" type="text/css" media="screen" />
+	<link rel="stylesheet" href="fancybox/source/helpers/jquery.fancybox-thumbs.css?v=1.0.7" type="text/css" media="screen" />
 
 </head>
 
@@ -57,73 +57,79 @@
 
   <div id="wrapper">
 
+	<!-- Mobile menu -->
   <div id="mobile_menu">
-    <img id="menu-logo" src="img/logo2.png" height="40" align="left">
-    <img id="menu-bars" src="img/menu_bars.png" height="32" align="right">
+	<img id="menu-logo" src="img/logo2.png" height="40" align="left">
+	<img id="menu-bars" src="img/menu_bars.png" height="32" align="right">
   </div>
 
+	<!-- Mobile dropdown -->
   <ul id="mobile_dropdown" style="display: none;">
-      <li><a href='index.php'><span>Home</span></a></li>
-      <li class="active"><a href='about.php'><span>About DECA</span></a></li>
-      <li><a href='events.php'><span>Events</span></a></li>
-      <li><a href='dashboard.php'><span>Dashboard</span></a></li>
-      <li><a href='announcements.php'><span>Announcements</span></a></li>
-      <li><a href='dates.php'><span>Schedules</span></a></li>
-      <?php if( !isset($_SESSION['user']) ) { ?>
-         <li><a href='register.php'><span>Register</span></a></li>
-         <li class='last'><a href='login.php'><span>Login</span></a></li>
-         <?php } else { ?>
-            <li><a href='exams.php'><span>Exams</span></a></li>
-           <li class='last'><a href='logout.php?logout'><span>Logout</span></a></li>
-        <?php
-      }
-      ?>
+	  <li><a href='index.php'><span>Home</span></a></li>
+	  <li class="active"><a href='about.php'><span>About DECA</span></a></li>
+	  <li><a href='events.php'><span>Events</span></a></li>
+	  <li><a href='dashboard.php'><span>Dashboard</span></a></li>
+	  <li><a href='announcements.php'><span>Announcements</span></a></li>
+	  <li><a href='dates.php'><span>Schedules</span></a></li>
+
+	<!-- Set menu according to login session -->
+	  <?php if( !isset($_SESSION['user']) ) { ?>
+		 <li><a href='register.php'><span>Register</span></a></li>
+		 <li class='last'><a href='login.php'><span>Login</span></a></li>
+		 <?php } else { ?>
+			<li><a href='exams.php'><span>Exams</span></a></li>
+		   <li class='last'><a href='logout.php?logout'><span>Logout</span></a></li>
+<?php
+}
+?>
   </ul>
 
-
+	<!-- Desktop menu -->
   <div id='cssmenu'>
-    <ul>
-        <li><a href='index.php'><span>Home</span></a></li>
-        <li><a href='about.php'><span>About DECA</span></a></li>
-        <li><a href='events.php'><span>Events</span></a></li>
-        <li><a href='dashboard.php'><span>Dashboard</span></a></li>
-        <li><a href='announcements.php'><span>Announcements</span></a></li>
-        <li><a href='dates.php'><span>Schedules</span></a></li>
-        <li class="active"><a href='exams.php'><span>Exams</span></a></li>
-        <li class='last'><a href='logout.php?logout'><span>Logout</span></a></li>
-    </ul>
+	<ul>
+		<li><a href='index.php'><span>Home</span></a></li>
+		<li><a href='about.php'><span>About DECA</span></a></li>
+		<li><a href='events.php'><span>Events</span></a></li>
+		<li><a href='dashboard.php'><span>Dashboard</span></a></li>
+		<li><a href='announcements.php'><span>Announcements</span></a></li>
+		<li><a href='dates.php'><span>Schedules</span></a></li>
+		<li class="active"><a href='exams.php'><span>Exams</span></a></li>
+		<li class='last'><a href='logout.php?logout'><span>Logout</span></a></li>
+	</ul>
   </div>
 </br>
 
-    <div class="content">
+	<div class="content">
 
 <h5>These exams are seperate from the homework - they are for your practice. You still need to complete the weekly exam in <a href="dashboard.php" style="color: black;">Dashboard.</a></h5>
 
-        <div class="breadcrumb">
-            <table class="breadcrumb">
-                <tr>
-                    <td id="clusterClicked"> </td>
-                    <td id="slash1" style="display: none;">/</td>
-                </tr>
-            </table>
-        </div>
+	<!-- Breadcrumb -->
+		<div class="breadcrumb">
+			<table class="breadcrumb">
+				<tr>
+					<td id="clusterClicked"> </td>
+					<td id="slash1" style="display: none;">/</td>
+				</tr>
+			</table>
+		</div>
 
-        <h4 id="table-title"></h4>
+	<!-- Table -->
+		<h4 id="table-title"></h4>
 
-        <table class="mdl-data-table mdl-js-data-table" id="table" width="100%">
-            <tbody>
+		<table class="mdl-data-table mdl-js-data-table" id="table" width="100%">
+			<tbody>
 
-            </tbody>
-        </table>
+			</tbody>
+		</table>
 
-    </div>
+	</div>
 
-
-    <div class="footer">
-        <img id="altlogo" src="img/logo_alt.png" align="left" />
-        <img id="fbimg" src="img/facebook-box.png" align="right" />
-        <h6><a href="http://www.irhsdeca.com" style="color: white;">irhsdeca.com</a></h6>
-    </div>
+	<!-- Footer -->
+	<div class="footer">
+		<img id="altlogo" src="img/logo_alt.png" align="left" />
+		<img id="fbimg" src="img/facebook-box.png" align="right" />
+		<h6><a href="http://www.irhsdeca.com" style="color: white;">irhsdeca.com</a></h6>
+	</div>
 </div>
 </body>
 
