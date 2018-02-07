b08d0b7bea71b27f230cf01bae895e1897ed193a
IbrahimIrfan <1ibrahimirfan@gmail.com>
2018-02-06 20:09:58 -0500

    Removed non-demo exams

--- Hospitality_1_Whole_Homework_Server.php
+++
@@ -1,1 +0,0 @@
-<?php ob_start(); session_start(); require_once 'dbconnect.php'; $score = $_POST["score"]; $UID = $_POST['user']; $query = "UPDATE exams SET score_1='$score', cluster='Hospitality' WHERE userId=".$UID;$res = mysql_query($query); ?>
--- Principles_5_Whole_Homework_Server.php
+++
@@ -1,1 +0,0 @@
-<?php ob_start(); session_start(); require_once 'dbconnect.php'; $score = $_POST["score"]; $UID = $_POST['user']; $query = "UPDATE exams SET score_5='$score', cluster='Principles' WHERE userId=".$UID;$res = mysql_query($query); ?>
