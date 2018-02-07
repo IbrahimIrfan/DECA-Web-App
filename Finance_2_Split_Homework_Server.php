a6d6356687a65cd35971e7fa10b2fb7c5422c676
Ibrahim Irfan <1ibrahimirfan@gmail.com>
2018-02-06 20:56:14 -0500

    Update Finance_2_Split_Homework_Server.php

--- Finance_2_Split_Homework_Server.php
+++ Finance_2_Split_Homework_Server.php
@@ -5,7 +5,7 @@
 
   // get score and user id
   $score = mysql_real_escape_string($_POST["score"]); 
-  $UID = mysql_real_escape_string($_POST['user']); 
+  $UID = mysql_real_escape_string($_POST["user"]); 
   $query = "UPDATE exams SET score_6='$score', cluster='Finance' WHERE userId=" . $UID;
   $res = mysql_query($query); 
 ?>
