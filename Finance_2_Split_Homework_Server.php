7ed201181e2146a8795ae5103be7f77f38b35394
Ibrahim Irfan <1ibrahimirfan@gmail.com>
2018-02-06 20:55:59 -0500

    Update Finance_2_Split_Homework_Server.php

--- Finance_2_Split_Homework_Server.php
+++ Finance_2_Split_Homework_Server.php
@@ -1,8 +1,11 @@
 <?php 
-ob_start(); 
-session_start(); 
-require_once 'dbconnect.php'; 
-$score = $_POST["score"]; 
-$UID = $_POST['user']; 
-$query = "UPDATE exams SET score_6='$score', cluster='Finance' WHERE userId=".$UID;
-$res = mysql_query($query); ?>
+  ob_start(); 
+  session_start(); 
+  require_once 'dbconnect.php'; 
+
+  // get score and user id
+  $score = mysql_real_escape_string($_POST["score"]); 
+  $UID = mysql_real_escape_string($_POST['user']); 
+  $query = "UPDATE exams SET score_6='$score', cluster='Finance' WHERE userId=" . $UID;
+  $res = mysql_query($query); 
+?>
