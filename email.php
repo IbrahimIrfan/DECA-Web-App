dc9954adcb8ef533b880a7b47fac69dc13daa0c0
Ibrahim Irfan <1ibrahimirfan@gmail.com>
2018-02-06 21:00:06 -0500

    Update email.php

--- email.php
+++ email.php
@@ -11,7 +11,7 @@ $mail->isSMTP();
 $mail->Host = 'smtp.gmail.com';
 $mail->SMTPAuth = true;
 $mail->Username = 'irhsdeca2016@gmail.com';
-$mail->Password = 'DECA2016';
+$mail->Password = '----------';
 $mail->SMTPSecure = 'tls';
 $mail->Port = 587;
 $mail->setFrom('irhsdeca2016@gmail.com', 'IRHS DECA');
