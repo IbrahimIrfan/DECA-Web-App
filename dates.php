6ca00d878c96c2e9f9e921dcd514127c2fca1f55
IbrahimIrfan <1ibrahimirfan@gmail.com>
2018-02-09 10:08:50 -0500

    Dates commenting

--- dates.php
+++ dates.php
@@ -1,7 +1,7 @@
 <?php
- ob_start();
- session_start();
- require_once 'dbconnect.php';
+	ob_start();
+	session_start();
+	require_once 'dbconnect.php';
 ?>
 
 <!--
@@ -49,108 +49,115 @@
 
   <div id="wrapper">
 
+  <!-- Mobile menu -->
   <div id="mobile_menu">
-    <img id="menu-logo" src="img/logo2.png" height="40" align="left">
-    <img id="menu-bars" src="img/menu_bars.png" height="32" align="right">
+	<img id="menu-logo" src="img/logo2.png" height="40" align="left">
+	<img id="menu-bars" src="img/menu_bars.png" height="32" align="right">
   </div>
 
+  <!-- Mobile dropdown -->
   <ul id="mobile_dropdown" style="display: none;">
-      <li><a href='index.php'><span>Home</span></a></li>
-      <li class="active"><a href='about.php'><span>About DECA</span></a></li>
-      <li><a href='events.php'><span>Events</span></a></li>
-      <li><a href='dashboard.php'><span>Dashboard</span></a></li>
-      <li><a href='announcements.php'><span>Announcements</span></a></li>
-      <li><a href='dates.php'><span>Schedules</span></a></li>
-      <?php if( !isset($_SESSION['user']) ) { ?>
-         <li><a href='register.php'><span>Register</span></a></li>
-         <li class='last'><a href='login.php'><span>Login</span></a></li>
-         <?php } else { ?>
-            <li><a href='exams.php'><span>Exams</span></a></li>
-           <li class='last'><a href='logout.php?logout'><span>Logout</span></a></li>
-        <?php
-      }
-      ?>
+	  <li><a href='index.php'><span>Home</span></a></li>
+	  <li class="active"><a href='about.php'><span>About DECA</span></a></li>
+	  <li><a href='events.php'><span>Events</span></a></li>
+	  <li><a href='dashboard.php'><span>Dashboard</span></a></li>
+	  <li><a href='announcements.php'><span>Announcements</span></a></li>
+	  <li><a href='dates.php'><span>Schedules</span></a></li>
+
+	<!-- Display menu according to user login -->
+	  <?php if( !isset($_SESSION['user']) ) { ?>
+		 <li><a href='register.php'><span>Register</span></a></li>
+		 <li class='last'><a href='login.php'><span>Login</span></a></li>
+		 <?php } else { ?>
+			<li><a href='exams.php'><span>Exams</span></a></li>
+		   <li class='last'><a href='logout.php?logout'><span>Logout</span></a></li>
+		<?php
+			}
+		?>
   </ul>
 
 
+  <!-- Desktop menu -->
   <div id='cssmenu'>
-      <ul>
-          <li><a href='index.php'><span>Home</span></a></li>
-          <li><a href='about.php'><span>About DECA</span></a></li>
-          <li><a href='events.php'><span>Events</span></a></li>
-          <li><a href='dashboard.php'><span>Dashboard</span></a></li>
-          <li><a href='announcements.php'><span>Announcements</span></a></li>
-          <li class='active'><a href='dates.php'><span>Schedules</span></a></li>
-          <?php if( !isset($_SESSION['user']) ) { ?>
-             <li><a href='register.php'><span>Register</span></a></li>
-             <li class='last'><a href='login.php'><span>Login</span></a></li>
-             <?php } else { ?>
-                <li><a href='exams.php'><span>Exams</span></a></li>
-               <li class='last'><a href='logout.php?logout'><span>Logout</span></a></li>
-            <?php
-          }
-          ?>
-      </ul>
+	  <ul>
+		  <li><a href='index.php'><span>Home</span></a></li>
+		  <li><a href='about.php'><span>About DECA</span></a></li>
+		  <li><a href='events.php'><span>Events</span></a></li>
+		  <li><a href='dashboard.php'><span>Dashboard</span></a></li>
+		  <li><a href='announcements.php'><span>Announcements</span></a></li>
+		  <li class='active'><a href='dates.php'><span>Schedules</span></a></li>
+		  <?php if( !isset($_SESSION['user']) ) { ?>
+			 <li><a href='register.php'><span>Register</span></a></li>
+			 <li class='last'><a href='login.php'><span>Login</span></a></li>
+			 <?php } else { ?>
+				<li><a href='exams.php'><span>Exams</span></a></li>
+			   <li class='last'><a href='logout.php?logout'><span>Logout</span></a></li>
+		<?php
+			}
+		?>
+	  </ul>
   </div>
   </br>
 
   <div class="content">
 
+<!-- show table with meeting schedule -->
   <h4>Meeting Schedule</h4>
-    <table class="mdl-data-table mdl-js-data-table" id="meetings" width="100%">
-      <thead id="table-heading">
-        <tr>
-            <td witdh="42%">Cluster</td><td width="28%">Day of Week</td><td width="25%">Executives</td>
-        </tr>
-      </thead>
-      <tbody>
-      <tr>      <td>Marketing (Teams)</td><td>Monday</td><td>Aazib and Melissa</td></tr>
-        <tr>            <td>Hospitality</td><td>Tuesday</td><td>Amy and Victoria</td></tr>
-      <tr>    <td>Finance</td><td>Wednesday</td><td>Jessica and Ezaan</td>
-        <tr>            <td>Business Admin</td><td>Wednesday</td><td>Natania and Jennifer</td></tr>
-  <tr>            <td>Marketing (Individual)</td><td>Thursday</td><td>Asdaq and Sophie</td></tr>
-    <tr>  <td>Principles</td><td>Thursday</td><td>Ibrahim and Laura</td> </tr>
-      </tbody>
-    </table>
+	<table class="mdl-data-table mdl-js-data-table" id="meetings" width="100%">
+	  <thead id="table-heading">
+		<tr>
+			<td witdh="42%">Cluster</td><td width="28%">Day of Week</td><td width="25%">Executives</td>
+		</tr>
+	  </thead>
+	  <tbody>
+	  	<tr><td>Marketing (Teams)</td><td>Monday</td><td>Aazib and Melissa</td></tr>
+		<tr><td>Hospitality</td><td>Tuesday</td><td>Amy and Victoria</td></tr>
+	  	<tr><td>Finance</td><td>Wednesday</td><td>Jessica and Ezaan</td>
+		<tr><td>Business Admin</td><td>Wednesday</td><td>Natania and Jennifer</td></tr>
+  		<tr><td>Marketing (Individual)</td><td>Thursday</td><td>Asdaq and Sophie</td></tr>
+		<tr><td>Principles</td><td>Thursday</td><td>Ibrahim and Laura</td></tr>
+	  </tbody>
+	</table>
 
+  <!-- Info for regionals -->
   <div class="regionals">
-    <h4 style="padding-left: 15px;">Regionals</h4>
-    <img src="img/location.png" align="left" width="50"/>
-    <h5 style="margin-top: 30px;">Hamilton Convention Centre</h5><br/>
-    <img src="img/date.png" align="left" width="50"/>
-    <h5 style="margin-top: 20px;">Exams: Oct 31, Case Studies: Nov 8</h5><br/>
-    <img src="img/money.png" align="left" height="50"/>
-    <h5 style="margin-top: 15px;">$37 Membership Fee + Regionals Fee</h5><br/>
+	<h4 style="padding-left: 15px;">Regionals</h4>
+	<img src="img/location.png" align="left" width="50"/>
+	<h5 style="margin-top: 30px;">Hamilton Convention Centre</h5><br/>
+	<img src="img/date.png" align="left" width="50"/>
+	<h5 style="margin-top: 20px;">Exams: Oct 31, Case Studies: Nov 8</h5><br/>
+	<img src="img/money.png" align="left" height="50"/>
+	<h5 style="margin-top: 15px;">$37 Membership Fee + Regionals Fee</h5><br/>
   </div>
 
+  <!-- Info for provincials -->
   <div class="provincials">
-    <h4 style="padding-left: 15px;">Provincials</h4>
-    <img src="img/location.png" align="left" width="50"/>
-    <h5 style="margin-top: 30px;">Toronto Sheraton Centre</h5><br/>
-    <img src="img/date.png" align="left" width="50"/>
-    <h5 style="margin-top: 20px;">February 9-11, 2017</h5><br/>
-    <img src="img/money.png" align="left" height="50"/>
-    <h5 style="margin-top: 15px;">$180 Provincial Fee + $115 Hotel Fee </h5><br/>
+	<h4 style="padding-left: 15px;">Provincials</h4>
+	<img src="img/location.png" align="left" width="50"/>
+	<h5 style="margin-top: 30px;">Toronto Sheraton Centre</h5><br/>
+	<img src="img/date.png" align="left" width="50"/>
+	<h5 style="margin-top: 20px;">February 9-11, 2017</h5><br/>
+	<img src="img/money.png" align="left" height="50"/>
+	<h5 style="margin-top: 15px;">$180 Provincial Fee + $115 Hotel Fee </h5><br/>
   </div>
 
+  <!-- Info for nationals -->
   <div class="icdc">
-      <h4 style="padding-left: 15px;">ICDC</h4>
-      <img src="img/location.png" align="left" width="50"/>
-      <h5 style="margin-top: 30px;">Anaheim, California</h5><br/>
-      <img src="img/date.png" align="left" width="50"/>
-      <h5 style="margin-top: 20px;">April 24 - May 1, 2017</h5><br/>
-      <img src="img/money.png" align="left" height="50"/>
-      <h5 style="margin-top: 15px;">TBD</h5><br/>
+	  <h4 style="padding-left: 15px;">ICDC</h4>
+	  <img src="img/location.png" align="left" width="50"/>
+	  <h5 style="margin-top: 30px;">Anaheim, California</h5><br/>
+	  <img src="img/date.png" align="left" width="50"/>
+	  <h5 style="margin-top: 20px;">April 24 - May 1, 2017</h5><br/>
+	  <img src="img/money.png" align="left" height="50"/>
+	  <h5 style="margin-top: 15px;">TBD</h5><br/>
   </div>
 
-
-
   </div>
 
-
+  <!-- Footer -->
   <div class="footer">
-    <img id="altlogo" src="img/logo_alt.png" align="left"/>
-    <img id="fbimg" src="img/facebook-box.png" align="right"/>
+	<img id="altlogo" src="img/logo_alt.png" align="left"/>
+	<img id="fbimg" src="img/facebook-box.png" align="right"/>
   <h6><a href="http://www.irhsdeca.com" style="color: white;">irhsdeca.com</a></h6>
   </div>
 </div>
@@ -159,7 +166,6 @@
   <script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
   <script defer src="https://code.getmdl.io/1.1.3/material.min.js"></script>
   <script src="js/dates.js" type="text/javascript"></script>
-
   <script type="text/javascript" src="js/menu.js"></script>
 
 
