b44d56f77f9a9cf927caaa7677cd9fafccff5ed8
IbrahimIrfan <1ibrahimirfan@gmail.com>
2018-02-09 10:24:17 -0500

    Logout comments

--- logout.php
+++ logout.php
@@ -1,17 +1,19 @@
 <?php
- session_start();
+	session_start();
 
- if (!isset($_SESSION['user'])) {
-  header("Location: login.php");
- } else if(isset($_SESSION['user'])!="") {
-  header("Location: dashboard.php");
- }
+	// if user is not logged in, redirect to login
+	// if login is not properly requested, redirect to dashboard
+	if (!isset($_SESSION['user'])) {
+		header("Location: login.php");
+	} else if(isset($_SESSION['user'])!="") {
+		header("Location: dashboard.php");
+	}
 
- if (isset($_GET['logout'])) {
-  session_destroy();
-  unset($_SESSION['user']);
-  header("Location: login.php");
-  exit;
- }
-
- ?>
+	// if login is requested, destroy session and redirect to login
+	if (isset($_GET['logout'])) {
+		session_destroy();
+		unset($_SESSION['user']);
+		header("Location: login.php");
+		exit;
+	}
+?>
