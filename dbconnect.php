7e84a0097173dd8721ec7c8c1f1aea9382b3af25
IbrahimIrfan <1ibrahimirfan@gmail.com>
2018-02-09 10:19:31 -0500

    dbconnect comments

--- dbconnect.php
+++ dbconnect.php
@@ -1,28 +1,30 @@
 <?php
+	// get the db values
+	$json = file_get_contents('http://www.irhsdeca.xyz/config.json');
+	$details = json_decode($json, true);
 
-  $json = file_get_contents('http://www.irhsdeca.xyz/config.json');
-  $details = json_decode($json, true);
+	// extract the details
+	$host = $details["dbhost"];
+	$user = $details["dbuser"];
+	$pass = $details["dbpass"];
+	$name = $details["dbname"];
 
- $host = $details["dbhost"];
- $user = $details["dbuser"];
- $pass = $details["dbpass"];
- $name = $details["dbname"];
+	// connect to db and error check
+	$conn = mysql_connect($host,$user,$pass);
+	$dbcon = mysql_select_db($name);
 
- $conn = mysql_connect($host,$user,$pass);
- $dbcon = mysql_select_db($name);
+	if (!$conn) {
+		die("Connection failed : " . mysql_error());
+	}
+	if (!$dbcon) {
+		die("Database Connection failed : " . mysql_error());
+	}
 
- if ( !$conn ) {
-  die("Connection failed : " . mysql_error());
- }
+	// set the timeout time to 120min (2hrs)
+	if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > 7200)) {
+		session_unset();     // unset $_SESSION variable for the run-time
+		session_destroy();   // destroy session data in storage
+	}
 
- if ( !$dbcon ) {
-  die("Database Connection failed : " . mysql_error());
-}
-
-if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > 7200)) {
-    session_unset();     // unset $_SESSION variable for the run-time
-    session_destroy();   // destroy session data in storage
-}
-$_SESSION['LAST_ACTIVITY'] = time(); // update last activity time stamp
-
+	$_SESSION['LAST_ACTIVITY'] = time(); // update last activity time stamp
 ?>
